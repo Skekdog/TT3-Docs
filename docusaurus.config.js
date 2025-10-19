93c41310662ff3a3866752f13881a8045929e42c
Skekdog <47750815+Skekdog@users.noreply.github.com>
2025-10-19 20:25:42 +0100

    for real this time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'images/social-card.jpg',
+      image: 'images/social-card.png',
       navbar: {
         title: 'Traitor Town 3',
         logo: {
