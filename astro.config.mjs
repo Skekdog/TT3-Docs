7a3c0fbfdbc7e21d8342d955b36e05a8d26a27bc
Gurkan <hello@gurkz.me>
2026-01-04 21:55:18 +0000

    starlight links validator
    Signed-off-by: Gurkan <hello@gurkz.me>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
       logo: {
         src: "./src/assets/logo.svg",
       },
+      plugins: [starlightLinksValidator()],
       components: {
         Head: "./src/components/Head.astro",
       },
