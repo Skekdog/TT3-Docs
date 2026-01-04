621025e0c8c9dc340a37ce0c4df581422f40fd3b
Gurkan <hello@gurkz.me>
2026-01-04 22:22:20 +0100

    add github link + view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,38 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://skekdog.github.io",
-	base: "/Traitor-Docs",
-	integrations: [
-		starlight({
-			title: 'Traitor! Docs',
-			favicon: "images/logo.svg",
-			logo: {
-				src: "./src/assets/logo.svg"
-			},
-			// social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://skekdog.github.io",
+  base: "/Traitor-Docs",
+  integrations: [
+    starlight({
+      title: "Traitor! Docs",
+      favicon: "images/logo.svg",
+      logo: {
+        src: "./src/assets/logo.svg",
+      },
+      components: {
+        Head: "./src/components/Head.astro",
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/Skekdog/Traitor-Docs",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
