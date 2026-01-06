47ac2b9692e3f0cdcdcdbed55ee8f538866492d3
Gurkan <hello@gurkz.me>
2026-01-06 13:29:27 +0100

    favicon needs to be in 'public'
    Signed-off-by: Gurkan <hello@gurkz.me>

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Traitor! Docs",
-      favicon: "./src/assets/logo.svg",
+      favicon: "/logo.svg",
       logo: {
         src: "./src/assets/logo.svg",
       },
